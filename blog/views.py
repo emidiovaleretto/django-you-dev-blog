acad2f23c93ce0fc57fa5b30bcdd3a92e76b540d
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-23 21:09:05 +0000

    Add PostDetail class and create a url path to link each post.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import *
 
 
 class PostList(generic.ListView):
@@ -8,3 +8,23 @@ class PostList(generic.ListView):
     queryset = model.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
+
+
+class PostDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.blog_comments.filter(approved=True).order_by('-created_on')
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context = {
+            'post': post,
+            'comments': comments,
+            'liked': liked
+        }
+
+        return render(request, 'post_detail.html', context=context)
