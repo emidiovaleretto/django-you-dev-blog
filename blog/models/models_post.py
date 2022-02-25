6c7c8a1ca18d38e79c4553226dfeef36bad9df2f
Emidio Valeretto <emidio.valereto@gmail.com>
2022-02-25 21:26:09 +0000

    Add an avatar field to post models.

--- blog/models/models_post.py
+++ blog/models/models_post.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
+    avatar = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
